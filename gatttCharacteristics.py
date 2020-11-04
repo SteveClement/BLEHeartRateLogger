c055dd46d1f8fb6cfd5bb0300a0c7fec2618fac4
Steve Clement <steve@localhost.lu>
2020-11-04 19:25:52 +0900

    new: [ble] Added BLE gattt characteristics

---
+++ gatttCharacteristics.py
@@ -0,0 +1,5 @@
+gatttChar = {}
+
+gatttChar["deviceName"] = "00002a00-0000-1000-8000-00805f9b34fb"
+gatttChar["manufacturer"] = "00002a29-0000-1000-8000-00805f9b34fb"
+gatttChar["battery"] = "00002a19-0000-1000-8000-00805f9b34fb"
